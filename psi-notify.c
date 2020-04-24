8d7dfe40264450d11575d4565c7fc8e1c1dbdd1d
Chris Down <chris@chrisdown.name>
2020-04-24 17:57:42 +0100

    Cache filename in SystemPressure

--- psi-notify.c
+++ psi-notify.c
@@ -1,5 +1,7 @@
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct {
     float some;
@@ -8,6 +10,7 @@ typedef struct {
 
 typedef struct {
     int available : 1;
+    char *filename;
 
     /* TRPs are only valid if available is true */
     TimeResourcePressure ten;
@@ -21,8 +24,50 @@ typedef struct {
     ResourcePressure io;
 } SystemPressure;
 
+char *get_pressure_file(char *resource) {
+    char *path;
+
+    path = malloc(PATH_MAX);
+
+    if (!path) {
+        perror("malloc");
+        abort();
+    }
+
+    /*
+     * If we have a logind seat for this user, use the pressure stats for that
+     * seat's slice. Otherwise, use the system-global pressure stats.
+     */
+    (void)snprintf(path, PATH_MAX,
+                   "/sys/fs/cgroup/user.slice/user-%d.slice/%s.pressure",
+                   getuid(), resource);
+    if (access(path, R_OK) == 0) {
+        return path;
+    }
+
+    (void)snprintf(path, PATH_MAX, "/proc/pressure/%s", resource);
+    if (access(path, R_OK) == 0) {
+        return path;
+    }
+
+    free(path);
+    return NULL;
+}
+
 SystemPressure *init_system_pressure(void) {
-    return calloc(1, sizeof(SystemPressure));
+    SystemPressure *sp;
+
+    sp = calloc(1, sizeof(SystemPressure));
+    if (!sp) {
+        perror("calloc");
+        abort();
+    }
+
+    sp->cpu.filename = get_pressure_file("cpu");
+    sp->memory.filename = get_pressure_file("memory");
+    sp->io.filename = get_pressure_file("io");
+
+    return sp;
 }
 
 int main(int argc, char *argv[]) {
